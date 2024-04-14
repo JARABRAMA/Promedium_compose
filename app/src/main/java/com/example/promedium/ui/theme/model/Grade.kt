d960dc130b208316fae49b9cbc61a977a711be57
JARABRAMA <brayanjaraba2006@gmail.com>
2024-04-14 05:49:58 -0500

    the data structures was improved

--- app/src/main/java/com/example/promedium/ui/theme/model/Grade.kt
+++ app/src/main/java/com/example/promedium/ui/theme/model/Grade.kt
@@ -4,4 +4,8 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Grade (var name: String, var qualification: Double, val percentage: Double): Parcelable
+data class Grade(var name: String, var qualification: Double, val percentage: Double) : Parcelable {
+    override fun toString(): String {
+        return "${this.name}$SEPARATOR${this.qualification}$SEPARATOR${this.percentage}"
+    }
+}
--- app/src/main/java/com/example/promedium/ui/theme/repository/SemesterRepository.kt
+++ app/src/main/java/com/example/promedium/ui/theme/repository/SemesterRepository.kt
@@ -1,6 +1,7 @@
 package com.example.promedium.ui.theme.repository
 
 import com.example.promedium.ui.theme.model.Course
+
 interface SemesterRepository {
     fun findAll(): MutableList<Course>
     fun add(course: Course): Course?

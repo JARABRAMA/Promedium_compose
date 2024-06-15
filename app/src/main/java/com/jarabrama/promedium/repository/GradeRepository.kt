9cdc274d6b8fc70e702ba6cc501bcc1c607613a9
JARABRAMA <brayanjaraba2006@gmail.com>
2024-06-15 15:05:00 -0500

    grade repository completed

--- app/src/main/java/com/jarabrama/promedium/repository/GradeRepository.kt
+++ app/src/main/java/com/jarabrama/promedium/repository/GradeRepository.kt
@@ -4,7 +4,7 @@ import com.jarabrama.promedium.model.Grade
 
 interface GradeRepository {
     fun findAll(courseId: Int): List<Grade>
-    fun save(grade: Grade): Grade
+    fun save(grades: MutableList<Grade>): Boolean
     fun delete(grade: Grade)
-    fun update(grades: List<Grade>)
+    fun get(id: Int): Grade?
 }
--- app/src/main/java/com/jarabrama/promedium/repository/impl/GradeRepositoryFileBased.kt
+++ app/src/main/java/com/jarabrama/promedium/repository/impl/GradeRepositoryFileBased.kt
@@ -9,6 +9,7 @@ import java.io.IOException
 import java.lang.Double.parseDouble
 import java.lang.Integer.parseInt
 import java.nio.charset.Charset
+import java.util.concurrent.Flow
 
 class GradeRepositoryFileBased(private val context: Context) : GradeRepository {
 
@@ -29,15 +30,23 @@ class GradeRepositoryFileBased(private val context: Context) : GradeRepository {
         }
     }
 
-    override fun findAll(courseId: Int): List<Grade> {
-        val allGrades = mutableListOf<Grade>()
-        context.openFileInput(fileAddress).use {
-            it.bufferedReader(Charset.defaultCharset()).forEachLine { line ->
-                allGrades.add(toGrade(line))
+    private fun getGrades(): MutableList<Grade> {
+        val grades: MutableList<Grade> = mutableListOf()
+        try {
+            context.openFileInput(fileAddress).bufferedReader().use { reader ->
+                reader.forEachLine { line ->
+                    grades.add(toGrade(line))
+                }
             }
+        } catch (e: IOException) {
+            Log.e("GradeRepositoryFileBased: getGrades", e.message, e)
         }
-        return allGrades.filter { it.courseId == courseId }
+        return grades
     }
+
+
+    override fun findAll(courseId: Int): List<Grade> = getGrades().filter { it.courseId == courseId }
+
 
     private fun toGrade(line: String): Grade {
         val parts: List<String> = line.split(separator)
@@ -51,22 +60,35 @@ class GradeRepositoryFileBased(private val context: Context) : GradeRepository {
     }
 
 
-    override fun save(grade: Grade): Grade {
-        TODO("Not yet implemented")
+    override fun save(grades: MutableList<Grade>): Boolean {
+        return try {
+            context.openFileOutput(fileAddress, Context.MODE_PRIVATE)
+                .bufferedWriter(Charset.defaultCharset()).use { writer ->
+                    grades.forEach { grade ->
+                        writer.write(toDb(grade))
+                        writer.newLine()
+                    }
+                }
+            true
+        } catch (e: IOException) {
+            Log.e("GradeRepositoryFileBased: Save", e.message, e)
+            false
+        }
+
     }
 
     override fun delete(grade: Grade) {
-        TODO("Not yet implemented")
-    }
-
-    override fun update(grades: List<Grade>) {
-        context.openFileOutput(fileAddress, Context.MODE_PRIVATE).bufferedWriter().use { writer ->
-            grades.forEach { grade ->
-                writer.write(toDb(grade))
-                writer.newLine()
-            }
+        val grades = getGrades()
+        if (grades.remove(grade)) {
+            save(grades)
+            Log.i("GradeRepositoryFileBased: delete", "grade ${grade.id} removed")
+        } else {
+            Log.e("GradeRepositoryFileBased: delete", "grade ${grade.id} not founded")
         }
     }
+
+    override fun get(id: Int): Grade? = getGrades().firstOrNull { course -> course.id == id }
+
 
     private fun toDb(grade: Grade): String {
         return "${grade.id}$separator${grade.courseId}$separator${grade.name}$separator${grade.qualification}$separator${grade.percentage}"
